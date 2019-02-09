7083ff60171c9cd070452699e3564184933673c6
Alexis Michaltsis <alexis@michaltsis.net>
2019-02-09 14:19:38 +0100

    Add redux store

--- posts_app_redux/src/App.js
+++ posts_app_redux/src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
 import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 
 import Posts from './components/Posts'
 import Postform from './components/Postform'
+
+const store = createStore(() => [], {}, applyMiddleware())
 
 export default class App extends Component {
   render() {
