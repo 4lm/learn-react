516eb6a38c6afe28e783b27ed81c83d612deba77
Alexis Michaltsis <alexis@michaltsis.net>
2019-02-09 14:10:04 +0100

    Make posts addable

--- posts_app_redux/src/App.js
+++ posts_app_redux/src/App.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-import Posts from './components/Posts';
+import Posts from './components/Posts'
+import Postform from './components/Postform'
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
+        <Postform />
+        <hr />
         <Posts />
       </div>
     );
   }
 }
-
-export default App;
