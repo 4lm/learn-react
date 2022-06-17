f557d74465e67b02805c7fa66ae0f91dd7dabc0a
Alexis Michaltsis <a.michaltsis@gmail.com>
2022-06-17 17:38:08 +0200

    Look into renders & re-renders

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -7,12 +7,15 @@ class App extends Component {
   constructor() {
     super();
 
+    console.log(1);
+
     this.state = {
       monsters: [],
     };
   }
 
   componentDidMount() {
+    console.log(3);
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
@@ -28,6 +31,7 @@ class App extends Component {
   }
 
   render() {
+    console.log(2);
     return (
       <div className="App">
         {this.state.monsters.map((monster) => {
