5c71bf1788eaa9b476d097abdca1723f410b3fd4
Alexis Michaltsis <a.michaltsis@gmail.com>
2022-06-22 15:05:43 +0200

    Enable searching & filtering

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   constructor() {
     super();
 
-    console.log(1);
+    console.log(1, "constructor");
 
     this.state = {
       monsters: [],
@@ -15,7 +15,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    console.log(3);
+    console.log(3, "componentDidMount");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
@@ -31,9 +31,25 @@ class App extends Component {
   }
 
   render() {
-    console.log(2);
+    console.log(2, "render");
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters..."
+          onChange={(event) => {
+            console.log("search:", event.target.value);
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            });
+          }}
+        />
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
