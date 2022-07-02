89f4da3e33d0bfa9341b2da261cb2be52f6b9891
Alexis Michaltsis <a.michaltsis@gmail.com>
2022-07-02 12:11:49 +0200

    Add toggle for dropdown

--- crwn-clothing/src/routes/navigation/navigation.component.jsx
+++ crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -6,10 +6,12 @@ import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <>
@@ -32,7 +34,7 @@ const Navigation = () => {
           )}
           <CartIcon />
         </div>
-        <CartDropdown />
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </>
