dcd2fecc2a6abb48b03e268b491f989be6cc5c53
Alexis Michaltsis <a.michaltsis@gmail.com>
2022-07-12 19:53:16 +0200

    Refactor categories context into categories Redux reducer

--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -8,18 +8,15 @@ import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { CartProvider } from "./contexts/cart.context";
-import { CategoriesProvider } from "./contexts/categories.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
