53068b47b566f09414cd05aa24ab2f14f668e3f1
Alexis Michaltsis <a.michaltsis@gmail.com>
2022-07-16 23:24:46 +0200

    Put self made logger in middleware module and again use former used redux logger

--- crwn-clothing/src/store/store.js
+++ crwn-clothing/src/store/store.js
@@ -1,23 +1,9 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-// import logger from "redux-logger";
+import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
-
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log("type: ", action.type);
-  console.log("payload: ", action.payload);
-  console.log("currentState: ", store.getState());
-
-  next(action);
-
-  console.log("next state: ", store.getState());
-};
 
 const persistConfig = {
   key: "root",
@@ -27,10 +13,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [
-  // logger,
-  loggerMiddleware,
-];
+const middleWares = [logger];
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 export const store = createStore(
