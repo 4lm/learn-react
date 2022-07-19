b751bc97967138d7a3bfb9767e1749718005c7fd
Alexis Michaltsis <a.michaltsis@gmail.com>
2022-07-19 23:17:17 +0200

    Switch from thunks to sagas for categories

--- crwn-clothing/src/store/store.js
+++ crwn-clothing/src/store/store.js
@@ -2,9 +2,10 @@ import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
-import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 
 import { rootReducer } from "./root-reducer";
+import { rootSaga } from "./root-saga";
 
 const persistConfig = {
   key: "root",
@@ -12,11 +13,11 @@ const persistConfig = {
   whitelist: ["cart"],
 };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const sagaMiddleware = createSagaMiddleware();
 
 const middleWares = [
   process.env.NODE_ENV !== "production" && logger,
-  thunk,
+  sagaMiddleware,
 ].filter(Boolean);
 
 const composeEnhancer =
@@ -25,6 +26,8 @@ const composeEnhancer =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
@@ -32,5 +35,7 @@ export const store = createStore(
   undefined,
   composedEnhancers
 );
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
